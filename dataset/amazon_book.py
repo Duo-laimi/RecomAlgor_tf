e87dbfedbca78b48fc43c1035013efc3365c577d
supeng <supengscu@qq.com>
2026-01-15 16:15:11 +0800

    feat: 加载amazon book数据集

---
+++ dataset/amazon_book.py
@@ -0,0 +1,75 @@
+import os
+from typing import Dict
+
+import pandas as pd
+
+from .base import DienDataset, Item
+
+"""
+-Amazon Book
+    - cat_voc.pkl
+    - mid_voc.pkl
+    - uid_voc.pkl
+    - local_train_splitByUser
+    - local_test_splitByUser
+    - reviews-info
+    - item-info
+"""
+
+# 从pkl文件中读取类型
+def read_from_pkl(path, astype=dict):
+    import pickle as pkl
+    with open(path, "rb") as f:
+        data = pkl.load(f)
+    if not isinstance(data, astype):
+        raise TypeError("Unmatched Type")
+    return data
+
+def read_codebooks(src: str):
+    codebooks = {}
+    codebook_items = ["cat_voc", "mid_voc", "uid_voc"]
+    for item in codebook_items:
+        if item.endswith(".pkl"):
+            item = item[:-4]
+        path = os.path.join(src, item + ".pkl")
+        codebooks[item] = read_from_pkl(path, astype=dict)
+    return codebooks
+
+def read_meta(path, codebooks):
+    cat_voc, mid_voc, uid_voc = list(codebooks.values())
+    meta_df = pd.read_csv(path, sep="\t", header=None, names=["mid", "cat"])
+    meta = {}
+    for row in meta_df.itertuples():
+        unique_id = row.mid
+        category = row.cat
+        mapping_id = mid_voc.get(unique_id, 0)
+        category_id = cat_voc.get(category, 0)
+        book = Item(unique_id, category, mapping_id, category_id)
+        meta[mapping_id] = book
+    return meta
+
+def load_amazon_book_df(path: str, codebooks: Dict[str, Dict[str, int]]):
+    # src = os.path.dirname(path)
+    # codebook_items = ["cat_voc", "mid_voc", "uid_voc"]
+    # codebooks = read_codebooks(src, codebook_items)
+    cat_voc, mid_voc, uid_voc = list(codebooks.values())
+
+    df = pd.read_csv(path, sep="\t", header=None, names=["label", "uid", "mid", "cat", "mid_str", "cat_str"])
+    df["mid_history"] = df["mid_str"].map(lambda x: [mid_voc[item] if item in mid_voc else 0 for item in x.split("\x02")])
+    df["cat_history"] = df["cat_str"].map(lambda x: [cat_voc[item] if item in cat_voc else 0 for item in x.split("\x02")])
+    df["uid"] = df["uid"].map(lambda item: uid_voc.get(item, 0))
+    df["mid"] = df["mid"].map(lambda item: mid_voc.get(item, 0))
+    df["cat"] = df["cat"].map(lambda item: cat_voc.get(item, 0))
+
+    return df
+
+
+def load_amazon_book(path: str, codebooks, meta):
+    df = load_amazon_book_df(path, codebooks)
+    ds = DienDataset(
+        source = df,
+        meta_mapping = meta,
+        codebooks = codebooks,
+        padding_length = 16
+    )
+    return ds
---
+++ dataset/base.py
@@ -0,0 +1,154 @@
+from dataclasses import dataclass
+from typing import List, Any, Dict, Optional
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+@dataclass
+class Item:
+    unique_id: str
+    category: str
+    mapping_id: int
+    category_id: int
+
+
+class DienDataset:
+    def __init__(
+            self,
+            source: pd.DataFrame,
+            meta_mapping: Dict[int, Item],
+            codebooks: Dict[str, Dict[str, int]],
+            pad_token: Any = 0,
+            padding_length: int = 32,
+            negative_sample: int = 5,
+            negative_prop: Optional[float] = None
+    ):
+        self.source = source
+        self.positive_keys = ["mid_history", "cat_history"]
+        self.keys = ["label", "uid", "mid", "cat", "mid_history", "cat_history"]
+        self.pad_token = pad_token
+        self.padding_length = padding_length
+
+        self.meta_mapping = meta_mapping
+        self.sorted_items = [meta_mapping[i] for i in range(len(meta_mapping))]
+
+        self.codebooks = codebooks
+        self.codebooks_keys = ["uid_voc", "mid_voc", "cat_voc"]
+
+        self.negative_prop = negative_prop
+        self.negative_sample = negative_sample
+
+        self.random_idx = list(range(self.source.shape[0]))
+
+    def __len__(self):
+        return self.source.shape[0]
+
+    def get_output_signature(self):
+        return (
+            tf.TensorSpec(shape=(), dtype=tf.float32), # label
+            tf.TensorSpec(shape=(), dtype=tf.int32), # uid
+            tf.TensorSpec(shape=(), dtype=tf.int32), # mid
+            tf.TensorSpec(shape=(), dtype=tf.int32), # cat
+            tf.TensorSpec(shape=(self.padding_length,), dtype=tf.int32), # mid_history
+            tf.TensorSpec(shape=(self.padding_length,), dtype=tf.int32),  # cat_history
+            tf.TensorSpec(shape=(), dtype=tf.int32),  # num_positives
+            tf.TensorSpec(shape=(self.padding_length, self.negative_sample), dtype=tf.int32), # neg_mid_history
+            tf.TensorSpec(shape=(self.padding_length, self.negative_sample), dtype=tf.int32)  # neg_cat_history
+        )
+
+    def padding_or_truncate(self, inputs: List[Any]):
+        extra = self.padding_length - len(inputs)
+        inputs = inputs + [self.pad_token] * extra
+        return inputs[:self.padding_length]
+
+    def negative_sampling(
+            self,
+            shape: tuple[int, ...],
+            positive_items: Optional[np.ndarray] = None,
+            sampling_weight: Optional[np.ndarray] = None,
+            replace: bool = False
+    ):
+        """
+        根据meta_mapping进行采样，位置编号要与映射编号一致
+
+        :param shape: 返回的采样形状，决定了采样的数量
+        :param positive_items: 采样结果中不能包含正样本
+        :param sampling_weight: 采样权重
+        :param replace: 是否可放回
+        :return: 采样结果
+        """
+        # 将正样本位置的采样权重设置为0
+        num_items = len(self.meta_mapping)
+        # sampling_weight与self.item_list的顺序一致
+        if sampling_weight is None:
+            sampling_weight = np.ones(num_items)
+        else:
+            sampling_weight = sampling_weight.copy()
+        if positive_items is not None:
+            sampling_weight[positive_items] = 0
+        sampling_weight[self.pad_token] = 0
+        sampling_weight = sampling_weight / sampling_weight.sum()
+        negatives = np.random.choice(num_items, shape, replace=replace, p=sampling_weight)
+        return negatives
+
+    def __getitem__(self, idx):
+        row = self.source.iloc[idx]
+        needed = []
+        positives = row["mid_history"]
+        num_positives = len(positives)
+
+        for key in self.keys:
+            item = row[key]
+            if key in self.positive_keys:
+                item = self.padding_or_truncate(item)
+            item = np.array(item)
+            needed.append(item)
+        needed.append(num_positives)
+        sampling_shape = (num_positives, self.negative_sample)
+        negatives = self.negative_sampling(sampling_shape, np.array(positives), sampling_weight=self.negative_prop)
+        negatives_cat = []
+        for idx in negatives.flatten():
+            negatives_cat.append(self.meta_mapping[idx].category_id)
+        negatives_cat = np.array(negatives_cat, dtype=np.int32).reshape(sampling_shape)
+        # 填充
+        pad = np.zeros((self.padding_length - num_positives, self.negative_sample))
+        negatives = np.concatenate([negatives, pad], axis=0)
+        negatives_cat = np.concatenate([negatives_cat, pad], axis=0)
+
+        needed.extend([negatives, negatives_cat])
+        return tuple(needed)
+
+class DienDatasetLoader:
+    def __init__(self, dataset: DienDataset, batch_size: int, shuffle: bool = True):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.next = 0
+        self.num_samples = len(dataset)
+        self.random_idx = list(range(self.num_samples))
+
+    def reset(self):
+        self.next = 0
+        if self.shuffle:
+            self.random_idx = np.random.permutation(self.num_samples).tolist()
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.next == self.num_samples:
+            self.next = 0
+            raise StopIteration
+        idx = self.random_idx[self.next]
+        self.next += 1
+        return self.dataset[idx]
+
+    def __call__(self):
+        return self.__next__
+
+
+
+
+
