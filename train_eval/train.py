84c0043f32624e7b5ce0c8c4158c00c97c1fc062
supeng <supengscu@qq.com>
2026-02-05 17:40:26 +0800

    统一从config获取参数

--- train_eval/train.py
+++ train_eval/train.py
@@ -54,12 +54,11 @@ def train(
 def train_from_config(config: Config):
     train_dataset = load_dataset(**config.data_config["train"])
     eval_dataset = load_dataset(**config.data_config["eval"])
-    training_args = config.training_config
-    set_all_seeds(training_args["seed"])
-    train_batch_size = training_args["train_batch_size"]
-    eval_batch_size = training_args["eval_batch_size"]
-    _train_loader = DienDatasetLoader(train_dataset, True, config.data_config["train_limit"])
-    _eval_loader = DienDatasetLoader(eval_dataset, True, config.data_config["eval_limit"])
+    set_all_seeds(config["seed"])
+    train_batch_size = config["train_batch_size"]
+    eval_batch_size = config["eval_batch_size"]
+    _train_loader = DienDatasetLoader(train_dataset, True, config["train_limit"])
+    _eval_loader = DienDatasetLoader(eval_dataset, True, config["eval_limit"])
     train_dataset_tf = tf.data.Dataset.from_generator(
         _train_loader,
         output_signature=_train_loader.get_output_signature()
@@ -75,32 +74,32 @@ def train_from_config(config: Config):
     MODEL_CLASS = getattr(parent_module, class_name)
     model = MODEL_CLASS(**config.model_config)
 
-    save_path = config.training_config["save_path"]
-    ckpt_path = config.training_config["ckpt_path"]
+    save_path = config["save_path"]
+    ckpt_path = config["ckpt_path"]
 
     name = config["name"]
     save_path = os.path.join(save_path, name)
 
-    from_scratch = config.training_config["from_scratch"]
+    from_scratch = config["from_scratch"]
     if os.path.exists(ckpt_path) and not from_scratch:
         # model.build(input_shape=(None, model.embedding_dim))
         # model.load_weights(ckpt_path)
         model = tf.keras.models.load_model(ckpt_path)
         logger.info(f"Training based on existing weights: {ckpt_path}.")
     else:
-        opt_name = training_args["optimizer"]
-        lr = training_args["learning_rate"]
-        weight_decay = training_args["weight_decay"]
+        opt_name = config["optimizer"]
+        lr = config["learning_rate"]
+        weight_decay = config["weight_decay"]
         opt = tf.keras.optimizers.get({"class_name": opt_name, "config": {"learning_rate": lr, "weight_decay": weight_decay}})
-        ls = tf.keras.losses.get(training_args['loss'])
-        met = [tf.keras.metrics.get(m) for m in training_args['metrics']]
+        ls = tf.keras.losses.get(config['loss'])
+        met = [tf.keras.metrics.get(m) for m in config['metrics']]
         model.compile(optimizer=opt, loss=ls, metrics=met)
 
     tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=f"logs/{name}", histogram_freq=1, update_freq=10)
     checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(ckpt_path, save_best_only=True)
     history = model.fit(
         train_dataset_tf,
-        epochs=training_args["num_epochs"],
+        epochs=config["num_epochs"],
         validation_data=eval_dataset_tf,
         callbacks=[tensorboard_cb, checkpoint_cb]
     )
