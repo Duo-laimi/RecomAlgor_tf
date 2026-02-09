33f28e256de9599b88987ba272c85ce86660440c
supeng <supengscu@qq.com>
2026-02-09 14:04:58 +0800

    简化调用过程

--- main.py
+++ main.py
@@ -1,82 +1,22 @@
-import os.path
 import logging as log
 
+import pandas as pd
+
 from parse_args import parse_args
-from train_eval.utils import set_all_seeds
-from utils.config import Config
+from train_eval.train import train_from_config
 from utils.common import setup_logging
-from dataset.impl.amazon_book import load_amazon_book as load_dataset
-from dataset.dien import DienDatasetLoader
-from  model.din import Din
+from utils.config import Config
 
-import tensorflow as tf
-from tensorflow.keras.losses import binary_crossentropy
-from tensorflow.keras.metrics import binary_accuracy, AUC, Recall, Precision
-import random
-import numpy as np
-
-logger = log.getLogger(__name__)
 
 def main(args):
     setup_logging(args.log_config)
-    config = Config(args.config)
-    train_dataset = load_dataset(**config.data_config["train"])
-    eval_dataset = load_dataset(**config.data_config["eval"])
-    training_args = config.training_config
-    set_all_seeds(training_args["seed"])
-    train_batch_size = training_args["train_batch_size"]
-    eval_batch_size = training_args["eval_batch_size"]
-    _train_loader = DienDatasetLoader(train_dataset, True)
-    _eval_loader = DienDatasetLoader(eval_dataset, True)
-    train_dataset_tf = tf.data.Dataset.from_generator(
-        _train_loader,
-        output_signature=_train_loader.get_output_signature()
-    ).shuffle(buffer_size=1024).batch(train_batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
-    eval_dataset_tf = tf.data.Dataset.from_generator(
-        _eval_loader,
-        output_signature=_eval_loader.get_output_signature()
-    ).batch(eval_batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
-    model_args = {
-        "num_users": train_dataset.num_users,
-        "num_items": train_dataset.num_items,
-        "num_categories": train_dataset.num_categories
-    }
-    config.model_config.update(model_args)
-    print(config.model_config)
-
-    model = Din(**config.model_config)
-    optimizer_args = training_args["optimizer_args"]
-
-    save_path = config.training_config["save_path"]
-    ckpt_path = config.training_config["ckpt_path"]
-    from_scratch = config.training_config["from_scratch"]
-    if os.path.exists(ckpt_path) and not from_scratch:
-        # model.build(input_shape=(None, model.embedding_dim))
-        # model.load_weights(ckpt_path)
-        model = tf.keras.models.load_model(ckpt_path)
-        logger.info(f"Training based on existing weights: {ckpt_path}.")
-    else:
-        model.compile(
-            optimizer=tf.keras.optimizers.Adam(**optimizer_args),
-            loss=binary_crossentropy,
-            metrics=[
-                binary_accuracy,
-                Precision(name="precision"),
-                Recall(name="recall"),
-                AUC(name="auc")
-            ]
-        )
-    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir="logs/din", histogram_freq=1, update_freq=10)
-    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(ckpt_path, save_best_only=True)
-    model.fit(
-        train_dataset_tf,
-        epochs=training_args["num_epochs"],
-        validation_data=eval_dataset_tf,
-        callbacks=[tensorboard_cb, checkpoint_cb]
-    )
-    # model.save(save_path)
-    model.export(save_path)
-
+    logger = log.getLogger(__name__)
+    cfg = Config(args.config)
+    val_metrics = train_from_config(cfg)
+    val_metrics["name"] = cfg["name"]
+    df = pd.DataFrame(val_metrics)
+    logger.info(df)
+    df.to_csv(f"{cfg['name']}.csv")
 
 
 if __name__ == "__main__":
